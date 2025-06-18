2442aa0e805d885b16d4be35faca082588f6efa5
Thomas Müller <thomas.mueller@dfinity.org>
2025-06-18 15:28:52 +0200

    PSEC-2173: Added support for V3 HTTP API

--- java-extension/src/main/java/org/dfinity/ic/burp/model/UrlPathInfo.java
+++ java-extension/src/main/java/org/dfinity/ic/burp/model/UrlPathInfo.java
@@ -2,18 +2,22 @@ package org.dfinity.ic.burp.model;
 
 import org.dfinity.ic.burp.tools.model.RequestType;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
 public record UrlPathInfo(String canisterId, RequestType requestType) {
-    private static final Pattern IC_API_PATH_REGEX = Pattern.compile("/api/v2/canister/(?<cid>[^/]+)/(?<rtype>query|call|read_state)");
+    private static final Pattern IC_API_V2_PATH_REGEX = Pattern.compile("/api/v2/canister/(?<cid>[^/]+)/(?<rtype>query|call|read_state)");
+    private static final Pattern IC_API_V3_PATH_REGEX = Pattern.compile("/api/v3/canister/(?<cid>[^/]+)/(?<rtype>call)");
 
     public static Optional<UrlPathInfo> tryFrom(String path) {
-        var matcher = IC_API_PATH_REGEX.matcher(path);
-        if (matcher.matches() && matcher.group("cid") != null && matcher.group("rtype") != null) {
-            try {
-                return Optional.of(new UrlPathInfo(matcher.group("cid"), RequestType.valueOf(matcher.group("rtype").toUpperCase())));
-            } catch (IllegalArgumentException ignored) {
+        for (var pathRegex : List.of(IC_API_V2_PATH_REGEX, IC_API_V3_PATH_REGEX)) {
+            var matcher = pathRegex.matcher(path);
+            if (matcher.matches() && matcher.group("cid") != null && matcher.group("rtype") != null) {
+                try {
+                    return Optional.of(new UrlPathInfo(matcher.group("cid"), RequestType.valueOf(matcher.group("rtype").toUpperCase())));
+                } catch (IllegalArgumentException ignored) {
+                }
             }
         }
         return Optional.empty();
