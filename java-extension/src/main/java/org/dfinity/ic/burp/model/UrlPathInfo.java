2b2f3b3c115babb8ee73b6a4b093b06356d478b7
Roel Storms <roel.storms@dfinity.org>
2026-02-11 14:45:09 +0100

    Added support for V4 HTTP API and extende V3 API support. (#25)

--- java-extension/src/main/java/org/dfinity/ic/burp/model/UrlPathInfo.java
+++ java-extension/src/main/java/org/dfinity/ic/burp/model/UrlPathInfo.java
@@ -8,10 +8,11 @@ import java.util.regex.Pattern;
 
 public record UrlPathInfo(String canisterId, RequestType requestType) {
     private static final Pattern IC_API_V2_PATH_REGEX = Pattern.compile("/api/v2/canister/(?<cid>[^/]+)/(?<rtype>query|call|read_state)");
-    private static final Pattern IC_API_V3_PATH_REGEX = Pattern.compile("/api/v3/canister/(?<cid>[^/]+)/(?<rtype>call)");
+    private static final Pattern IC_API_V3_PATH_REGEX = Pattern.compile("/api/v3/canister/(?<cid>[^/]+)/(?<rtype>query|call|read_state)");
+    private static final Pattern IC_API_V4_PATH_REGEX = Pattern.compile("/api/v4/canister/(?<cid>[^/]+)/(?<rtype>call)");
 
     public static Optional<UrlPathInfo> tryFrom(String path) {
-        for (var pathRegex : List.of(IC_API_V2_PATH_REGEX, IC_API_V3_PATH_REGEX)) {
+        for (var pathRegex : List.of(IC_API_V2_PATH_REGEX, IC_API_V3_PATH_REGEX, IC_API_V4_PATH_REGEX)) {
             var matcher = pathRegex.matcher(path);
             if (matcher.matches() && matcher.group("cid") != null && matcher.group("rtype") != null) {
                 try {
